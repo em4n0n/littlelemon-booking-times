be2a2ea3cc48ffeb6ba8f0d596ce267927d0ed7a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-03-17 13:19:55 -0700

    create a menu model

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -8,3 +8,9 @@ class Booking(models.Model):
     
     def __str__(self):
         return self.first_name
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    menu_item_description = models.TextField(max_length=1000, default='')
+    
